90492146b5c98b81c62bcbccf3ff117f605eed0f
KennethIMAvila <avilakennethivan@gmail.com>
2024-10-19 15:30:21 +0800

    Login not complete

--- App.js
+++ App.js
@@ -1,12 +1,26 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Image
+        style={{width: 100, height: 100, resizeMode: 'contain'}}
+        source={require('./assets/ESP.jpg')}
+      />
+      <Text style={{...styles.header, fontSize: 20, fontWeight: 'bold', fontFamily: 'Inter-Bold'}}>Welcome to ESP app</Text>
+      <Text style={{marginBottom: 1}}></Text>
+      <Text style={{fontFamily: 'Gowun Dodum', textAlign: 'center', marginBottom: 3}}>Learn values that guide good decisions,</Text>
+      <Text style={{fontFamily: 'Gowun Dodum', textAlign: 'center', marginBottom: 3}}>build respect, and help you grow in all</Text>
+      <Text style={{fontFamily: 'Gowun Dodum', textAlign: 'center', marginBottom: 3}}>parts of life. Edukasyon sa Pagpapakatao</Text>
+      <Text style={{fontFamily: 'Gowun Dodum', textAlign: 'center', marginBottom: 3}}>teaches us how to live with honesty,</Text>
+      <Text style={{fontFamily: 'Gowun Dodum', textAlign: 'center', marginBottom: 3}}>kindness, and integrity.</Text>
+      <View style={{width: 280, height: 50, backgroundColor: '#1E73BE', borderRadius: 50, justifyContent: 'center', alignItems: 'center'}}>
+        <Text style={{color: '#fff', fontFamily: 'Inter-Bold', fontSize: 16, fontWeight:'bold'}}>Login</Text>
+      </View>
       <StatusBar style="auto" />
     </View>
+    
   );
 }
 
